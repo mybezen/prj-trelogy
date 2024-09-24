5fc1882cc6e7edf526a5d5b8e64a579c6d0b8ffa
mybezen <amriikhda207@gmail.com>
2024-09-24 15:38:34 +0700

    NAVBAR NGEBUG WOI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,24 @@
 import { BreadcrumbsWithIcon } from "../components/Breadcrumbs";
+import { NavbarWithMegaMenu } from "../components/Navbar";
 import ScrollB from "../components/ScrollBox";
 
 function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-[#EAE6E3]">
+      <NavbarWithMegaMenu />
       {/* Main content that grows to push the footer down */}
-      <div className="flex-grow">
+      <div className="items-center justify-center flex-grow">
         <BreadcrumbsWithIcon />
         <h1>Hello World</h1>
       </div>
 
       {/* Footer */}
-      <footer className="bg-teal-500 w-full h-24 flex items-center justify-center">
+      <footer className="flex items-center justify-center w-full h-24 bg-teal-500">
         <p>Footer Content</p>
       </footer>
       <div>
         <ScrollB />
-      </div> 
+      </div>
     </div>
   );
 }
