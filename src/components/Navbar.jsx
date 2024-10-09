779dfbaf687c31cfbe93fa4d96e3b8ab19d21a0b
mybezen <amriikhda207@gmail.com>
2024-10-09 07:23:39 +0700

    Lang Change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,38 @@
 function CenteredNavbar() {
-  return(
+  return (
     <header className="flex justify-center p-4">
-
-    <nav className="flex items-center max-w-md px-6 py-2 space-x-6 bg-white rounded-full bg-opacity-20 plus-jakarta-sans-bold">
-            <a href="/" className="hover:text-gray-200">
-              Home
-            </a>
-            <a href="/about" className="hover:text-gray-200">
-              About
-            </a>
-            <a href="#" className="hover:text-gray-200">
-              Blog
-            </a>
-            <a href="#" className="hover:text-gray-200">
-              Contact
-            </a>
-            <svg
-          className="w-6 h-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4 6h16M4 12h16M4 18h16"
-          />
-        </svg>
-          </nav>
+      <nav className="flex items-center max-w-md px-6 py-2 space-x-6 bg-white rounded-full bg-opacity-20 plus-jakarta-sans-bold">
+        <a href="/" className="hover:text-gray-200">
+          Beranda
+        </a>
+        <a href="/ikrar" className="hover:text-gray-200">
+          Tentang
+        </a>
+        <a href="/strug" className="hover:text-gray-200">
+          Blog
+        </a>
+        <a href="#" className="hover:text-gray-200">
+          Kontak
+        </a>
+        <a href="/">
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </a>
+      </nav>
     </header>
-  )
+  );
 }
 
 export default CenteredNavbar;
