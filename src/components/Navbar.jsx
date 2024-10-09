80ef7d963df703f6f055a4b404a18cffdc435a7d
awiipp <sayidariipp@gmail.com>
2024-10-09 18:47:43 +0700

    edit navbar and title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Logo from '/assets/Logo.svg';
 function CenteredNavbar() {
   return (
     <header className="relative flex justify-center p-4">
-      <nav className="flex items-center justify-center max-w-md px-6 py-2 md:space-x-6 space-x-4 bg-white rounded-full bg-opacity-60 plus-jakarta-sans-bold">
+      <nav className="flex items-center justify-center max-w-md px-6 py-2 text-red-800 md:space-x-6 space-x-4 bg-white rounded-full bg-opacity-30 plus-jakarta-sans-bold">
         <img className="h-[4vh]" src={Logo}></img>
         <a href="/" className="hover:text-gray-200">
           Beranda
