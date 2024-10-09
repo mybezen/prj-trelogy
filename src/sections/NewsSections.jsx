12f69cb44146d6fb6ca6167e27c78ae40ddac387
awiipp <sayidariipp@gmail.com>
2024-10-09 11:56:06 +0700

    fix desktop view

--- src/sections/NewsSections.jsx
+++ src/sections/NewsSections.jsx
@@ -1,14 +1,14 @@
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Default carousel stylesimport "./carouselCustom.css"; // Custom styles
-import SlideIcon1 from '../assets/BuildingImages/IC.png';
-import SlideIcon2 from '../assets/BuildingImages/OJB.png';
-import SlideIcon3 from '../assets/BuildingImages/JJB.jpg';
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Default carousel stylesimport "./carouselCustom.css"; // Custom styles
+import SlideIcon1 from "../assets/BuildingImages/IC.png";
+import SlideIcon2 from "../assets/BuildingImages/OJB.png";
+import SlideIcon3 from "../assets/BuildingImages/JJB.jpg";
 
 function ImageSection() {
   return (
-    <div className="flex items-center w-full mx-1 md:mx-0">
+    <div className="flex items-center md:w-10/12 mx-1 md:mx-0">
       <div className="flex flex-col w-5/12 md:items-end">
-        <div className="md:w-[300px] md:h-[200px] overflow-hidden rounded-lg ml-2 mb-2">
+        <div className="md:w-[300px] md:h-[215px] overflow-hidden rounded-lg ml-2 mb-2">
           <img
             src={SlideIcon1}
             alt="Lorong Zaman"
@@ -16,7 +16,7 @@ function ImageSection() {
           />
         </div>
 
-        <div className="md:w-[300px] md:h-[200px] overflow-hidden rounded-lg ml-2">
+        <div className="md:w-[300px] md:h-[215px] overflow-hidden rounded-lg ml-2">
           <img
             src={SlideIcon2}
             alt="Kolom"
@@ -25,7 +25,7 @@ function ImageSection() {
         </div>
       </div>
 
-      <div className="md:w-[37.5rem] md:h-[25.625rem] overflow-hidden rounded-lg m-2 w-7/12">
+      <div className="md:w-[37.5rem] overflow-hidden rounded-lg m-2 w-7/12">
         <Carousel
           showThumbs={false}
           infiniteLoop
@@ -48,7 +48,7 @@ function ImageSection() {
               alt="Pablo Neruda 2"
               className="w-full h-full object-cover bg-center"
             />
-            <p className="legend">Pablo Journey</p>
+            <p className="legend -mt-52">Pablo Journey</p>
           </div>
           <div className="w-full h-full">
             <img
